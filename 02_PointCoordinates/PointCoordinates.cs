ce0560a9f8b2d3d8ee52c77dfb07160d52e42ede
AnastasiyaTolopilo <anastasiya.tolopilo@gmail.com>
2023-01-05 22:12:15 +0100

    Added point coordinates class and creacted ten points;

---
+++ 02_PointCoordinates/PointCoordinates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_PointCoordinates
+{
+    internal class PointCoordinates
+    {
+        public int X;
+        public int Y;
+
+        public string GetCoordinates()
+        {
+            return X + " " + Y;
+        }
+    }
+}
---
+++ 02_PointCoordinates/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _02_PointCoordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointCoordinates point1 = new PointCoordinates();
+            point1.X = 100;
+            point1.Y = 10;
+
+            PointCoordinates point2 = new PointCoordinates();
+            point2.X = 200;
+            point2.Y = 20;
+
+            PointCoordinates point3 = new PointCoordinates();
+            point3.X = 300;
+            point3.Y = 30;
+
+            PointCoordinates point4 = new PointCoordinates();
+            point4.X = 400;
+            point4.Y = 40;
+
+            PointCoordinates point5 = new PointCoordinates();
+            point5.X = 500;
+            point5.Y = 50;
+
+            PointCoordinates point6 = new PointCoordinates();
+            point6.X = 600;
+            point6.Y = 60;
+
+            PointCoordinates point7 = new PointCoordinates();
+            point7.X = 700;
+            point7.Y = 70;
+
+            PointCoordinates point8 = new PointCoordinates();
+            point8.X = 800;
+            point8.Y = 80;
+
+            PointCoordinates point9 = new PointCoordinates();
+            point9.X = 900;
+            point9.Y = 90;
+
+            PointCoordinates point10 = new PointCoordinates();
+            point10.X = 1000;
+            point10.Y = 100;
+
+            PointCoordinates[] points = { point1, point2, point3, point4, point6, point7, point8, point9, point10 };
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine(points[i].GetCoordinates());
+            }
+        }
+    }
+}
